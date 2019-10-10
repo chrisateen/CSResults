e4bc69e725af99fe906d6b0b709c162b6f7c1e78
chrisateen <cawofe01@dcs.bbk.ac.uk>
2019-10-10 22:57:39 +0100

    Added a context class for the models

---
+++ CSResults/DAL/ModuleContext.cs
@@ -0,0 +1,19 @@
+﻿using CSResults.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CSResults.DAL
+{
+    public class ModuleContext:DbContext
+    {
+        public ModuleContext() : base("ModuleContext")
+        {
+        }
+
+        public DbSet<Module> Module { get; set; }
+        public DbSet<Result> Result { get; set; }
+    }
+}
