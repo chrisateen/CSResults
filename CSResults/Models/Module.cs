243072c29e5343b2393284b755e2c6e5661f8a5a
chrisateen <cawofe01@dcs.bbk.ac.uk>
2019-10-10 22:51:24 +0100

    Added module model

--- CSResults/Models/Module.cs
+++ CSResults/Models/Module.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CSResults.Models
 {
     public class Module
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string moduleID { get; set; }
+        public string moduleName { get; set; }
+
+        public virtual ICollection<Result> Results { get; set; }
     }
 }
