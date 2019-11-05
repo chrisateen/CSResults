f0c406bc42c8db2fb805b57d71aaa21a07aa50e4
chrisateen <cawofe01@dcs.bbk.ac.uk>
2019-11-05 21:35:26 +0000

    Results Graph View Model added

---
+++ CSResults/Models/ResultsGraphViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSResults.Models
+{
+
+    /*
+     * View Model to enable to select all modules for a dropdown list and the filtered module data
+    */
+    public class ResultsGraphViewModel
+    {
+        public IEnumerable<Module> modules { get; set; }
+        public IEnumerable<ResultsViewModel> resultViewModel { get; set; }
+    }
+}
