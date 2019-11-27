0dbada2202f30457664b33717022abe9dd292657
chrisateen <cawofe01@dcs.bbk.ac.uk>
2019-11-27 21:33:13 +0000

    Added generic interface using repository pattern

---
+++ CSResults/DAL/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSResults.DAL
+{
+    public interface IGenericRepository<T> where T:class
+    {
+        IEnumerable<T> GetAll();
+
+        T GetByModName(string modName);
+
+        T GetByModID(string modID);
+    }
+}
