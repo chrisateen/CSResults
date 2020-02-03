d05674e8ceac181186913c88f4bb5e1164af6fff
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-02-03 23:10:02 +0000

    Removed IModuleContext class

--- CSResults/DAL/ModuleContext.cs
+++ CSResults/DAL/ModuleContext.cs
@@ -7,7 +7,7 @@ using System.Web;
 
 namespace CSResults.DAL
 {
-    public class ModuleContext:DbContext,IModuleContext
+    public class ModuleContext:DbContext
     {
         public ModuleContext() : base("ModuleContext")
         {
