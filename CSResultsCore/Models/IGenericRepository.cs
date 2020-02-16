a9cee73feb3ec31265cf57299bd8653f02d6a338
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-02-16 22:45:10 +0000

    Added IGeneric repository class

---
+++ CSResultsCore/Models/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSResultsCore.Models
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll(Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                                params Expression<Func<T, object>>[] includes);
+
+        IEnumerable<T> Get();
+        IEnumerable<T> Get(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            params Expression<Func<T, object>>[] includes);
+    }
+}
