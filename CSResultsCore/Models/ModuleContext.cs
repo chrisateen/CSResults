cb698a400fdeebff48604574b75844bc5337fd6c
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-02-16 22:14:54 +0000

    Module Context added

---
+++ CSResultsCore/Models/ModuleContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using CSResultsCore.Models;
+
+
+namespace CSResults.DAL
+{
+    public class ModuleContext:DbContext
+    {
+        public ModuleContext(DbContextOptions<ModuleContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<Result> Results { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Result>()
+                .HasOne<Module>(r => r.Module)
+            .WithMany(m => m.Results)
+            .HasForeignKey(m => m.moduleID);
+        }
+    }
+}
