e83370b74d62a2ceeaa0fba8969a1284ae53bde1
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-02-16 22:15:15 +0000

    Namespace updated on models

--- CSResultsCore/Models/Module.cs
+++ CSResultsCore/Models/Module.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel;
 
-namespace CSResults.Models
+namespace CSResultsCore.Models
 {
     public class Module
     {
