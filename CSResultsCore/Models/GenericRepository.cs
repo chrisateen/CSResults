4bbfc8d8e2cae0603c8d9eee7f3954aae25f2b8b
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-04-25 22:22:08 +0100

    Added genericRepository Class

---
+++ CSResultsCore/Models/GenericRepository.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace CSResultsCore.Models
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private DbContext _context;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<T> Get()
+        {
+            return _context.Set<T>().ToList();
+        }
+        public IEnumerable<T> GetAll(Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                                        params Expression<Func<T, object>>[] includes)
+        {
+            DbSet<T> dbSet = _context.Set<T>();
+            IQueryable<T> query = dbSet;
+
+            //Checks if we have objects to include/merge into our query
+            if (includes.Length > 0)
+            {
+                foreach (var include in includes)
+                {
+                    query = dbSet.Include(include);
+                }
+
+            }
+
+            //Orders the query if there is an orderby specifed
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            return query.ToList();
+
+
+        }
+
+        public IEnumerable<T> Get(
+                        Expression<Func<T, bool>> filter = null,
+                        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                        params Expression<Func<T, object>>[] includes)
+        {
+            DbSet<T> dbSet = _context.Set<T>();
+            IQueryable<T> query = dbSet;
+
+            //Checks if we have objects to include/merge into our query
+            if (includes.Length > 0)
+            {
+                foreach (var include in includes)
+                {
+                    query = dbSet.Include(include);
+                }
+            }
+
+            //Filter the query if there is a filter condition
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            //Order the query if there is an orderby condition
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            var res = query.ToList();
+            return res;
+        }
+
+    }
+}
