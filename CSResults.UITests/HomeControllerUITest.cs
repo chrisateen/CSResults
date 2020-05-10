c5205ffa56579597d04e73ed8163908e5f84c148
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-05-10 18:12:30 +0100

    Added UI Tests project

---
+++ CSResults.UITests/HomeControllerUITest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CSResults.UITests
+{
+    
+    public class HomeControllerUITest
+    {
+        [Fact]
+        public void LoadHomePage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://localhost:44368/");
+            }
+        }
+    }
+}
---
+++ CSResults.UITests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CSResults.UITests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
